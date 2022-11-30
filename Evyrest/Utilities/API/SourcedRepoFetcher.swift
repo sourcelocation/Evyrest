533fed2326315414cd7cfb20288bddf29bd7bc72
Matthew <anisovich.matthew@gmail.com>
2022-11-30 19:59:58 +0000

    i hate you llsc

--- Evyrest/Utilities/API/SourcedRepoFetcher.swift
+++ Evyrest/Utilities/API/SourcedRepoFetcher.swift
@@ -21,6 +21,6 @@ class SourcedRepoFetcher: ObservableObject {
         request.addValue("Basic \(authBase64)", forHTTPHeaderField: "Authorization")
         let (data, response) = try await session.data(for: request) as! (Data, HTTPURLResponse)
         
-        print(response)
+//        print(response)
     }
 }
