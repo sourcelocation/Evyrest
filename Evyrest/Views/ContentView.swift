ad203feff9bf565bc6a125115ce10741b434db5d
Lakhan Lothiyi <42747613+llsc12@users.noreply.github.com>
2022-11-30 19:29:09 +0000

    :trollface:

--- Evyrest/Views/ContentView.swift
+++ Evyrest/Views/ContentView.swift
@@ -7,77 +7,23 @@
 
 import SwiftUI
 
-
 struct ContentView: View {
-    @ObservedObject var imageSourcing = ImageSourcing(apiSource: .pexels)
-    @AppStorage("userToken") var userToken: String?
+    @ObservedObject var imageSourcing = ImageSourcing.shared
+    @KeychainStorage("userToken") var userToken: String?
     
     var body: some View {
-        ZStack {
-            VStack {
-                Image("Evyrest")
-                    .resizable()
-                    .frame(width: 120.0, height: 120.0)
-                    .padding(.bottom, 40.0)
-                
-                ZStack {
-                    VStack {
-                        ForEach(ImageSourcing.APISource.allCases, id: \.rawValue) { sourceType in
-                            VStack {
-                                Button {
-                                    imageSourcing.apiSource = sourceType
-                                } label: {
-                                    HStack {
-                                        Image(sourceType.rawValue)
-                                            .resizable()
-                                            .aspectRatio(contentMode: .fit)
-                                            .padding(.trailing, 20.0)
-                                            .frame(height: 32)
-                                        Text(sourceType.rawValue)
-                                            .foregroundColor(Color.white)
-                                            .padding(.trailing, 20.0)
-                                        Spacer()
-                                        Image(systemName: imageSourcing.apiSource == sourceType ? "checkmark.circle.fill" : "circle")
-                                            .resizable()
-                                            .aspectRatio(contentMode: .fit)
-//                                            .foregroundColor(.accentColor)
-                                            .frame(height: 22)
-                                            .symbolRenderingMode(.palette)
-                                            .foregroundStyle(Color.white, Color.accentColor)
-                                            .opacity(imageSourcing.apiSource == sourceType ? 1 : 0.25)
-                                    }
-                                    .padding(.vertical, 10)
-                                    .frame(maxWidth: .infinity)
-//                                    .background(.thinMaterial.opacity(imageSourcing.apiSource == sourceType ? 0.25 : 0))
-//                                    .cornerRadius(100)
-                                    .padding(.horizontal)
-                                    .animation(.spring().speed(2), value: imageSourcing.apiSource)
-                                }
-                                .buttonStyle(.plain)
-                                if ImageSourcing.APISource.allCases.last! != sourceType {
-                                    Divider()
-                                        .background(.white)
-                                }
-                            }
-                        }
-                    }
-                    .padding()
-                }
-                .background(.ultraThinMaterial.opacity(0.5))
-                .cornerRadius(20)
-                .frame(maxWidth: 300)
-            }
-            Spacer()
+        VStack {
+            Image("Evyrest")
+                .padding(.horizontal)
         }
-        .background(
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background {
             Image("Background")
                 .resizable()
-                .aspectRatio(contentMode: .fill)
-                .edgesIgnoringSafeArea(.all)
-                .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+                .scaledToFill()
+                .ignoresSafeArea()
                 .blur(radius: 5)
-                .scaleEffect(1.1)
-        )
+        }
     }
 }
 
