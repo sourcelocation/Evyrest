f0648cf292d126446d01569241f946e457f777d9
haxi0 <85764897+haxi0@users.noreply.github.com>
2022-12-17 21:55:14 +0300

    finally works at least somehow

--- Evyrest/Views/ContentView.swift
+++ Evyrest/Views/ContentView.swift
@@ -7,39 +7,135 @@
 
 import SwiftUI
 import CoreMotion
+import SDWebImageSwiftUI
 
 struct ContentView: View {
     @ObservedObject var imageSourcing = ImageSourcing.shared
+    @ObservedObject var imageManager = ImageManager()
+    @Environment(\.openURL) var openURL
     @KeychainStorage("userToken") var userToken: String?
     @AppStorage("enabled") var enabled: Bool = false
+    @State private var fadeOut = false
     
     var body: some View {
-        VStack {
-            header
-            Spacer()
-            sourceLocation
-            Spacer()
-            Spacer()
-            Spacer()
-            button
-        }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .overlay(alignment: .topTrailing, content: {
-            Text("gn")
-        })
-        .background {
-            Image("Background")
-                .resizable()
-                .scaledToFill()
-                .blur(radius: 5)
-                .ignoresSafeArea()
-                .parallaxed(magnitude: 1.5)
-        }
-        .background(Color("BackgroundColor"))
-        .safeAreaInset(edge: .bottom) {
-            Text(copyrightLine)
-                .foregroundColor(.white)
-                .font(.footnote)
+        if enabled {
+            NavigationView {
+                Form {
+                    HStack {
+                        if imageManager.image != nil {
+                            Spacer()
+                            Image(uiImage: imageManager.image!)
+                                .resizable()
+                                .frame(width: 300, height: 300)
+                                .clipShape(RoundedRectangle(cornerRadius: 40))
+                                .opacity(fadeOut ? 0 : 1) // Animation on background.
+                                .animation(.easeInOut(duration: 0.25), value: fadeOut) // Animation on background.
+                            Spacer()
+                        } else {
+                            Spacer()
+                            ProgressView()
+                                .padding()
+                            Spacer()
+                        }
+                    }
+                    Section {
+                        Label {
+                            HStack {
+                                Button("Refresh") {
+                                    self.fadeOut.toggle()
+                                    SDImageCache.shared.clearMemory()
+                                    SDImageCache.shared.clearDisk()
+                                    
+                                    self.imageManager.load(url: URL(string: "https://source.unsplash.com/random/"))
+                                    
+                                    DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                                        withAnimation {
+                                            self.fadeOut.toggle()
+                                            print("Done!")
+                                        }
+                                    }
+                                }
+                            }
+                        } icon: {
+                            Image("RefreshIcon")
+                                .resizable()
+                                .frame(width: 30, height: 30)
+                                .clipShape(RoundedRectangle(cornerRadius: 7))
+                        }
+                        Label {
+                            HStack {
+                                Button("Set as Wallpaper") {
+                                    UIImageWriteToSavedPhotosAlbum(imageManager.image!, nil, nil, nil)
+                                    Timer.scheduledTimer(withTimeInterval: 1, repeats: false) { _ in
+                                        openURL(URL(string: "shortcuts://run-shortcut?name=Evyrest")!)
+                                    }
+                                }
+                            }
+                        } icon: {
+                            Image("WallpaperIcon")
+                                .resizable()
+                                .frame(width: 30, height: 30)
+                                .clipShape(RoundedRectangle(cornerRadius: 7))
+                        }
+                        .disabled(fadeOut)
+                        Label {
+                            HStack {
+                                Button("Exit") {
+                                    enabled.toggle()
+                                }
+                                .foregroundColor(Color.red)
+                            }
+                        } icon: {
+                            Image("ExitIcon")
+                                .resizable()
+                                .frame(width: 30, height: 30)
+                                .clipShape(RoundedRectangle(cornerRadius: 7))
+                        }
+                    }
+                }
+                // Trigger image loading when appear
+                .onAppear {
+                    self.fadeOut.toggle()
+                    SDImageCache.shared.clearMemory()
+                    SDImageCache.shared.clearDisk()
+                    
+                    self.imageManager.load(url: URL(string: "https://source.unsplash.com/random/"))
+                    
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                        withAnimation {
+                            self.fadeOut.toggle()
+                            print("Done!")
+                        }
+                    }
+                }
+                // Cancel image loading when disappear
+                .onDisappear { self.imageManager.cancel() }
+            }
+        } else {
+            VStack {
+                header
+                Spacer()
+                sourceLocation
+                Spacer()
+                Spacer()
+                Spacer()
+                button
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background {
+                Image("Background")
+                    .resizable()
+                    .scaledToFill()
+                    .blur(radius: 5)
+                    .ignoresSafeArea()
+                    .parallaxed(magnitude: 1.5)
+            }
+            .background(Color("BackgroundColor"))
+            .safeAreaInset(edge: .bottom) {
+                Text(copyrightLine)
+                    .foregroundColor(.white)
+                    .font(.footnote)
+            }
         }
     }
     
@@ -104,7 +200,7 @@ struct ContentView: View {
         Button(action: {
             enabled.toggle()
         }) {
-            Image(systemName: enabled ? "checkmark" : "xmark")
+            Image(systemName: enabled ? "xmark" : "checkmark")
                 .foregroundColor(.white)
                 .padding()
                 .frame(maxWidth: 300)
